0ed60338c61f42b3e09bebf6e190c7cba3a7dfa1
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-29 16:52:50 -0800

    Recording module display notes

--- src/Piano/PianoSystem.java
+++ src/Piano/PianoSystem.java
@@ -24,24 +24,15 @@ public class PianoSystem {
 	MidiTrack noteTrack = new MidiTrack();
 	Instant momentZero;
 	
-	//determines whether the key presses should be tracked
 	private boolean recording;
 	
 	private ArrayList<Scale> scales=new ArrayList<Scale>();
 	
-	//contains the int values that correspond to the notes
 	private int[] noteValue;
-	
-	//contains one boolean per key to keep track of which is clicked
-	//private boolean[] clickedValue;
-	
+		
 	private ArrayList<PianoKey> pianoKeys=new ArrayList<>();
 	
-	//Contains the String constants that correspond to the keyboard
 	private final String[] KEYBOARD_KEYS={"TAB","ONE","Q","TWO","W","E","FOUR","R","FIVE","T","SIX","Y","U","EIGHT","I","NINE","O","P","MINUS","RBRACKET","EQUALS","LBRACKET","BACKSPACE","SLASH","HOME"};
-	/*
-	 * Constructor checks if the file exists, if not then creates one
-	 */
 	public PianoSystem(){
 		TimeSignature ts = new TimeSignature();
 		ts.setTimeSignature(4, 4, TimeSignature.DEFAULT_METER, TimeSignature.DEFAULT_DIVISION);
@@ -51,11 +42,7 @@ public class PianoSystem {
 
 		tempoTrack.insertEvent(ts);
 		tempoTrack.insertEvent(t);
-		
-		
-		
-		
-		
+	
 		noteValue=new int[25];
 		int temp=53;
 		for(int i=0;i<25;i++){
@@ -64,7 +51,6 @@ public class PianoSystem {
 			noteValue[i]=temp;
 			temp++;
 		}
-		
 		
 		this.f=new File("scales.txt");
 		if(!f.isFile()){
@@ -81,10 +67,6 @@ public class PianoSystem {
 		}
 			
 	}
-	/**
-	 * Reads the contents of the file and re populates them into an ArrayList 
-	 * 
-	 */
 	public void update(){
 		try {
 			Scanner input=new Scanner(f);
@@ -98,22 +80,14 @@ public class PianoSystem {
 		
 		
 	}
-	/*
-	 * Adds a scale to the database of scales
-	 */
 	public void addScale(Scale s) {
 		scales.add(s);
 		try {
 			writeFile();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	/**
-	 * Saves the current ArrayList of scales to a file
-	 * @throws FileNotFoundException 
-	 */
 	private void writeFile() throws FileNotFoundException{
 		PrintWriter pw = new PrintWriter(new FileOutputStream("scales.txt"));
 		for(int i=0;i<scales.size();i++){
@@ -123,40 +97,14 @@ public class PianoSystem {
 		}
 		pw.close();
 	}
-	/**
-	 * 
-	 * 
-	 * @param val takes in a String value and returns an int corresponding to the noteValue
-	 * @return integer ranging from 53 to 75 or -1 if invalid
-	 */
-	public int getNoteValue(String val){
-//		for(int i=0;i<KEYBOARD_KEYS.length;i++){
-//			if(KEYBOARD_KEYS[i].equals(val))
-//				return noteValue[i];	
-//		}
-		
+	public int getNoteValue(String val){		
 		for(int i=0;i<pianoKeys.size();i++){
 			if(pianoKeys.get(i).getKeyBoardVal().equals(val))
 				return pianoKeys.get(i).getMidiValue();	
 		}
-		
-		
-		
 		return -1;
-		
 	}
-	/**
-	 * Goes to the list of booleans and sets the index at given value to true
-	 * 
-	 * @param val String ; the key you want to change
-	 * @param b true for clicked, false for not
-	 */
-	public void setKeyClicked(String val,boolean b){
-//		for(int i=0;i<KEYBOARD_KEYS.length;i++){
-//			if(KEYBOARD_KEYS[i].equals(val))
-//				clickedValue[i]=b;	
-//		}
-		
+	public void setKeyClicked(String val,boolean b){	
 		for(int i=0;i<pianoKeys.size();i++){
 			if(pianoKeys.get(i).getKeyBoardVal().equals(val)){
 				pianoKeys.get(i).setClicked(b);
@@ -241,10 +189,6 @@ public class PianoSystem {
 
 		return melody;
 	}
-	/**
-	 * Tells the system whether it should track the key presses 
-	 * @param rec true to track, false to not
-	 */
 	public void setRecording(boolean rec){
 		this.recording=rec;
 		momentZero=Instant.now();
