848109cf107d256ef41c5bb4917110e26b3701e9
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-29 16:50:33 -0800

    Added support for saving midi files

--- src/Piano/PianoKey.java
+++ src/Piano/PianoKey.java
@@ -2,19 +2,11 @@ package Piano;
 
 import java.time.Instant;
 
-//class models a physical piano key
 public class PianoKey {
 	private int midiValue;
 	private String keyBoardVal;
 	private boolean isClicked;
 	 private Instant begin, end;
-	/**
-	 * this class models a piano's keys
-	 * 
-	 * @param keyValue the integer value that java uses
-	 * @param keyBoardVal the keyBoard name that is associated with the note
-	 * @param isClicked boolean to check if the key is clicked
-	 */
 	public PianoKey(int midiValue, String keyBoardVal, boolean isClicked) {
 		super();
 		this.midiValue = midiValue;
@@ -27,10 +19,6 @@ public class PianoKey {
 	public void setMidiValue(int keyValue) {
 		this.midiValue = keyValue;
 	}
-	/**
-	 * 
-	 * @return the name of the key associated with that note
-	 */
 	public String getKeyBoardVal() {
 		return keyBoardVal;
 	}
@@ -40,20 +28,12 @@ public class PianoKey {
 	public boolean isClicked() {
 		return isClicked;
 	}
-	/**
-	 * Set if the key is being pressed
-	 * @param isClicked
-	 */
 	public void setClicked(boolean isClicked) {
 		this.isClicked = isClicked;
 	}
 	public Instant getBegin() {
 		return begin;
 	}
-	/**
-	 * sets the instant when the key was first pressed
-	 * @param begin
-	 */
 	public void setBegin() {
 		this.begin = Instant.now();
 	}
